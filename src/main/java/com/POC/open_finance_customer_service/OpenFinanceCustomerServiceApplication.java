f6af061de104e99ea8a888d6d81214ef3f77fbc8
Victor Fernandes <victor.fernandes.07@gmail.com>
2025-07-13 10:32:24 -0300

    init: create Spring Boot reactive service with MongoDB and Docker setup

---
+++ src/main/java/com/POC/open_finance_customer_service/OpenFinanceCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.POC.open_finance_customer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFinanceCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFinanceCustomerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/POC/open_finance_customer_service/OpenFinanceCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.POC.open_finance_customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFinanceCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
